5777697c440f956daa9ec7a7370aedf025f5f6ea
Niladri <rickyrickado14@gmail.com>
2023-07-09 17:15:02 +0530

    login page created with controllers

--- client/src/index.js
+++ client/src/index.js
@@ -3,22 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Navbar from './Navbar';
+import Landing from './Landing';
 import About from './About';
 import Service from './Service';
 import Footer from './Footer';
+import StreamJoin from './StreamJoin';
+import Stream from './Stream';
 import 'font-awesome/css/font-awesome.min.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App/> */}
+    <App/> 
     {/* <StreamJoin/> */}
-    {/* <Stream/> */}
-    <Navbar/>
+     {/*<Stream/>*/} 
+   {/* <Navbar/>
+    <Landing/>
     <About/>
     <Service/>
-    <Footer/>
-
+    <Footer/>*/}
+    
   </React.StrictMode>
 );
