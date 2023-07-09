e79b506e5264f118864817b5f886d8369f4c5d16
Priyanshu <priyanshurouth@gmail.com>
2023-07-09 13:20:32 +0530

    Mongo DB Connected User Schema made

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 const express= require("express")
-const dotenv= require('dotenv')
+const dotenv= require('dotenv').config()
+const connectDb = require('./configDatabase/database');
 const cors = require('cors');
 const http= require('http')
 const app= express()
 const server= http.createServer(app)
 const port= 3000
+connectDb()
+app.use(cors({
+    origin: 'http://localhost:3001'
+  }));
+  
 server.listen(port, ()=>{
     console.log(`Server is running on port ${port}`);
 })
