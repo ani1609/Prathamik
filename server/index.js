7912919ee431142532a0fff469cd68d76b783169
Priyanshu <priyanshurouth@gmail.com>
2023-07-09 12:34:24 +0530

    dot env created server improved

--- server/index.js
+++ server/index.js
@@ -1,4 +1,10 @@
 const express= require("express")
+const dotenv= require('dotenv')
+const cors = require('cors');
+const http= require('http')
 const app= express()
-app.listen(3000)
-console.log("server Running on port 3000")
+const server= http.createServer(app)
+const port= 3000
+server.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`);
+})
