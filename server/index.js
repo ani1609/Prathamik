807f2966fe021d0230c676a8470b4d3b070aba5a
Priyanshu <priyanshurouth@gmail.com>
2023-07-09 14:57:38 +0530

    login signup completed

--- server/index.js
+++ server/index.js
@@ -3,10 +3,15 @@ const dotenv= require('dotenv').config()
 const connectDb = require('./configDatabase/database');
 const cors = require('cors');
 const http= require('http')
+const signupController = require('./controllers/signup');
+const loginController = require('./controllers/login');
 const app= express()
 const server= http.createServer(app)
 const port= 3000
 connectDb()
+//routes
+app.use('/signup', signupController);
+app.use('/login', loginController);
 app.use(cors({
     origin: 'http://localhost:3001'
   }));
