265589435b126230d60d1e5185379abf5db986f8
Priyanshu <priyanshurouth@gmail.com>
2023-07-09 17:36:41 +0530

    Navbar Updated

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,15 +1,23 @@
+import { Link } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
 import './Navbar.css';
 import './index.css';
 
-function Navbar() 
+function Navbar()
 {
   const [navbarShadow, setNavbarShadow] = useState(false);
+  const [navTabscolor, setNavTabsColor]= useState(false);
+  const [loginSignupColor, setLoginSignupColor]= useState(false);
   const showShadow = () => {
-    if (window.scrollY > 50) {
+    if (window.scrollY > 50)
+    {
+      setNavTabsColor(true);
+      setLoginSignupColor(true);
       setNavbarShadow(true);
     }
     else {
+      setNavTabsColor(false);
+      setLoginSignupColor(false);
       setNavbarShadow(false);
     }
   }
@@ -23,15 +31,15 @@ function Navbar()
             <h1>Prathamik</h1>
         </a>
 
-        <ul className='navigations-tabs'>
+        <ul className={navTabscolor?'navigations-tabs nav-tabs-color':'navigations-tabs nav-tabs-white'}>
           <li><a href="/">HOME</a></li>
-          <li><a href="/">ABOUT US</a></li>
-          <li><a href="/">SERVICE</a></li>
-          <li><a href="/">BLOG</a></li>
+          <Link className='li' to="/platform">Platform</Link>
+          <Link className='li' to="/stream">Stream</Link>
+          <Link className='li' to="/streamjoin">StreamJoin</Link>
           <li>
             <div className='login-signup-container'>
-              <button type="button" className='login'>LOG IN</button>
-              <button type="button" className='signup'>SIGN UP</button>
+              <Link to="/login" className={loginSignupColor?'login login-color' : 'login login-white'}>LOG IN</Link>
+              <Link to="/signup" className={loginSignupColor?'signup signup-color' : 'signup signup-white'}>SIGN UP</Link>
             </div>
           </li>
         </ul>
@@ -39,6 +47,4 @@ function Navbar()
     </div>
   );
 }
-
 export default Navbar;
-
