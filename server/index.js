6cacf6cdabaddb9354f00967fdc6646a71f14526
Priyanshu <priyanshurouth@gmail.com>
2023-07-09 17:15:55 +0530

    Signalling server linked with main server

--- server/index.js
+++ server/index.js
@@ -1,21 +1,37 @@
-const express= require("express")
-const dotenv= require('dotenv').config()
 const connectDb = require('./configDatabase/database');
+const File=require('./models/file');
 const cors = require('cors');
-const http= require('http')
-const signupController = require('./controllers/signup');
-const loginController = require('./controllers/login');
-const app= express()
-const server= http.createServer(app)
-const port= 3000
-connectDb()
-//routes
-app.use('/signup', signupController);
-app.use('/login', loginController);
+const express = require('express');
+const app = express();
+connectDb();
+const http = require('http');
+const { Server } = require('socket.io');
+const { handleUpgrade, handleWebSocketConnection, initializeSignalingServer } = require('./stream/streamrtc');
+const routes = require('./Routes/routes');
+const { handleInput } = require('./gpt-3.5/gptController/inputController.js');
+
+app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:3001'
-  }));
-  
-server.listen(port, ()=>{
-    console.log(`Server is running on port ${port}`);
-})
+  origin: 'http://localhost:3001'
+}));
+
+app.use('/api', routes);
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3001',
+    methods: ['GET', 'POST']
+  }
+});
+initializeSignalingServer(io);
+io.on('connection', handleWebSocketConnection);
+
+server.on('upgrade', handleUpgrade);
+
+app.post('/input', handleInput);
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
