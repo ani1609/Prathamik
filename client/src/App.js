2b15b320d8ee9dc588d8c14f74316ebb2a103f16
Niladri <rickyrickado14@gmail.com>
2023-07-09 22:01:31 +0530

    integrated whiteboard

--- client/src/App.js
+++ client/src/App.js
@@ -7,37 +7,39 @@ import About from './About';
 import Service from './Service';
 import Footer from './Footer';
 import Platform from './Platform';
-import Whiteboard from './whiteboard'
 import Stream from './Stream';
-import StreamJoin from './StreamJoin';
 import Signup from './Signup';
 import Login from './Login';
 import 'font-awesome/css/font-awesome.min.css';
+import { useState } from 'react';
+import Container from './Container';
+import io from "socket.io-client";
+
+const socket = io.connect("http://localhost:3000");
 
 function App() {
+  const [isAdmin, setIsAdmin] = useState(false);
   return (
     <Router>
       <div>
         <Routes>
-          <Route exact path="/" element={<Home />} />
+          <Route exact path="/" element={<Home setIsAdmin={setIsAdmin} />} />
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/signup" element={<Signup />} />
-          <Route exact path="/:id" element={<Platform />} />
+          <Route exact path="/:id" element={<Platform isAdmin={isAdmin} socket={socket} />} />
           <Route exact path="/stream" element={<Stream />} />
-          <Route exact path="/streamjoin" element={<StreamJoin />} />
-          <Route exact path="/board" element={<Whiteboard/>}/>
-
+          <Route exact path='/whiteboard' element={<Container socket={socket} />} />
         </Routes>
       </div>
     </Router>
   );
 }
 
-function Home() {
+function Home(props) {
   return (
     <div>
       <Navbar />
-      <Landing />
+      <Landing setIsAdmin={props.setIsAdmin}  />
       <About />
       <Service />
       <Footer />
